ce9b154878eb45ae21147cd821453c33d2dea7ef
Rajveer6000 <rajveerjaat7300@gmail.com>
2026-06-04 19:46:44 +0530

    Add Vercel Analytics integration and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="hotel-yuvaan-theme">
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 );
