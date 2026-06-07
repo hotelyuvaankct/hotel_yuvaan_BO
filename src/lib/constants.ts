73e3ae05ab9cb8e7f4493f31adceef713c5c85fd
hotelyuvaankct <hotelyuvaankct@gmail.com>
2026-06-07 16:03:44 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = 'https://api.hotelyuvaan.com/api/v1.0.0';
-export const API_BASE_URL = 'http://localhost:8080/api/v1.0.0';
+export const API_BASE_URL = 'https://api.hotelyuvaan.com/api/v1.0.0';
+// export const API_BASE_URL = 'http://localhost:8080/api/v1.0.0';
     
