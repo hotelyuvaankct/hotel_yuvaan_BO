78ce1826fd98e45ae0d435a5e27e5b76367ef6b4
Rajveer6000 <rajveerjaat7300@gmail.com>
2026-06-14 22:41:58 +0530

    fix: update API_BASE_URL to production endpoint and comment out local development URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://api.hotelyuvaan.com/api/v1.0.0';
-export const API_BASE_URL = 'http://localhost:8080/api/v1.0.0';
+export const API_BASE_URL = 'https://api.hotelyuvaan.com/api/v1.0.0';
+// export const API_BASE_URL = 'http://localhost:8080/api/v1.0.0';
 
 export enum Status {
     ACTIVE = 1,
